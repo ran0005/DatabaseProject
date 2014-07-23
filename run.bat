84d43885149fa1c7ef6461252680f81d6708be81
Robert T Baldwin <robbaldwin95@gmail.com>
2014-07-23 14:55:07 -0500

    Batch works!

--- run.bat
+++ run.bat
@@ -1,2 +1,14 @@
-javac -cp /lib/postgresql.jdbc4.jar -d bin project/project/* project/tables/*
-java -cp .:/lib/postgresql.jdbc4.jar;bin/ project.Comp5120
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+(
+
+	cd $ROOT
+
+	# Use gradle! It's great! See this for an example: https://github.com/vmrob/aciddb-java-client
+	# Note the "build.gradle" file
+
+
+	javac -cp lib/postgresql.jdbc41.jar -d bin project/project/* project/tables/*
+	java -cp "lib/*;bin" project.Comp5120
+
+)
