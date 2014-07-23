215726f2c5ec2415aaa6d20fc010a06bf4cc1331
jam0058 <jam0058@auburn.edu>
2014-07-23 14:51:22 -0500

    Update ericsQueries.sql

--- ericsQueries.sql
+++ ericsQueries.sql
@@ -1,6 +1,6 @@
 /* A1. List the rooms that are occupied, along with the associated patient names 
 and the date the patient was admitted. */
-select roomNum, pFirstName, pMInit, pLastName, startTime
+select roomNum, pLastName || ', ' || pFirstName || ' ' || pMInit as Name, startTime
 from patient join
 (
 	select roomNum, patID, startTime
@@ -11,7 +11,8 @@ from patient join
 		where patID IS NOT NULL
 	) as a1 using (patID)
 	where endTime IS NULL
-) as a2 using (patID);
+) as a2 using (patID)
+order by roomNum;
 
 -- A2. List the rooms that are currently unoccupied.
 select roomNum
@@ -20,7 +21,7 @@ where patID IS NULL;
 
 /* A3. List all rooms in the hospital along with patient names and admission dates 
 for those that are occupied. */
-select roomNum, pFirstName, pMInit, pLastName, startTime
+select roomNum, pLastName || ', ' || pFirstName || ' ' || pMInit as Name, startTime
 from room left join
 (
 	select patID, pFirstName, pMInit, pLastName, startTime
@@ -39,7 +40,7 @@ select treatID, tName, occurrences
 from treatment join
 (
 	select treatID, COUNT(treatID) as occurrences
-	from administers join
+	from adminsters join
 	(
 		select patID
 		from admit
@@ -48,6 +49,29 @@ from treatment join
 	group by treatID
 ) as t1 using (treatID)
 order by occurrences;
+
+/* C7. List the diagnoses associated with patients who have the highest occurrences 
+of admissions to the hospital, in ascending order or correlation. */
+select dName
+from diagnosis join
+(
+	select diagID
+	from admit join
+	(
+		select patID, COUNT(patID) as admissions
+		from admit
+		where admissions = (select MAX(admissions) from (
+			select patID, COUNT(patID) as admissions
+			from admit
+			group by patID
+			) as a1)
+		group by patID
+	) as a2 using (patID)
+) as d1 using (diagID);
+
+---
+--OR
+---
 
 /* C7. List the diagnoses associated with patients who have the highest occurrences 
 of admissions to the hospital, in ascending order or correlation. */
@@ -72,3 +96,25 @@ order by count(dName);
 
 /* C8. For a given treatment occurrence, list all the hospital employees that were 
 involved. Also include the patient name and the doctor who ordered the treatment. */
+select docName, patName,
+eLastName || ', ' || eFirstName || ' ' || eMInit as empName
+from employee join
+(
+	select eLastName || ', ' || eFirstName || ' ' || eMInit as docName,
+	patName,
+	adminID
+	from employee join
+	(
+		select pLastName || ', ' || pFirstName || ' ' || pMInit as patName,
+		docID,
+		adminID
+		from patient join
+		(
+			select empID as docID, empAdministerID as adminID, patID
+			from Orders join
+			Administers using (orderID)
+			where orderID = ?
+		) o1 using (patID)
+	) as p1 on (docID = empID)
+) as e2 on (adminID = empID);
+
