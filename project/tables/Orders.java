f263409c81514fffd8038eaa5aed43cf3619cc2d
Ian <Typhoan@users.noreply.github.com>
2014-07-23 14:27:05 -0500

    Update Orders.java

--- project/tables/Orders.java
+++ project/tables/Orders.java
@@ -16,25 +16,30 @@ public class Orders extends Table {
 	public void getPreparedStatement(BufferedReader br, PreparedStatement pst)
 			throws IOException, NumberFormatException, SQLException {
 		String temp = "";
-
-		System.out.print("Input value for " + "empID" + ": ");
+      
+      System.out.print("Input value for " + "OrderID" + ": ");
 		temp = br.readLine();
-
-		pst.setInt(1, Integer.parseInt(temp));
-
-		System.out.print("Input value for " + "treatID" + ": ");
+      
+      pst.setInt(1, Integer.parseInt(temp));
+      
+		System.out.print("Input value for " + "empID" + ": ");
 		temp = br.readLine();
 
 		pst.setInt(2, Integer.parseInt(temp));
 
-		System.out.print("Input value for " + "patID" + ": ");
+		System.out.print("Input value for " + "treatID" + ": ");
 		temp = br.readLine();
 
 		pst.setInt(3, Integer.parseInt(temp));
 
+		System.out.print("Input value for " + "patID" + ": ");
+		temp = br.readLine();
+
+		pst.setInt(4, Integer.parseInt(temp));
+
 		System.out.print("Input value for " + "orderTime" + ": ");
 		temp = br.readLine();
 
-		pst.setString(4, temp);
+		pst.setString(5, temp);
 	}
 }
