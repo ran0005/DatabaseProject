65375fa3409d222d370f8b84491b430d71909f07
Ian <Typhoan@users.noreply.github.com>
2014-07-25 14:34:53 -0500

    Create U3.java

---
+++ project/tables/U3.java
@@ -0,0 +1,48 @@
+package tables;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class U3 extends UpdateTable {
+	private Connection con;
+	private static int pID;
+   
+	public U3(Connection con) {
+		this.con = con;
+	}
+   
+   public int getPID(){
+      return pID;
+   }
+   
+	public PreparedStatement prepareStatement() {
+		try {
+			return con.prepareStatement(qU3);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public String getStatement() {
+		return qU3;
+	}
+	
+	public void getPreparedStatement(BufferedReader br, PreparedStatement pst) throws IOException, NumberFormatException, SQLException {
+		String temp = "";
+		
+		System.out.println("Please enter Diagnosis ID: ");
+		temp = br.readLine();
+		pst.setInt(1, Integer.parseInt(temp));
+      
+      System.out.println("Please enter patient ID to checkout: ");
+      temp = br.readLine();
+		pst.setInt(2, Integer.parseInt(temp));
+	}	
+   
+   
+}
