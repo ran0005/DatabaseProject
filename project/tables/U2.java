5553b4a9080f4f7ae834547c841f6ea8f74fa47f
Ian <Typhoan@users.noreply.github.com>
2014-07-25 14:33:36 -0500

    Create U2.java

---
+++ project/tables/U2.java
@@ -0,0 +1,39 @@
+package tables;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class U2 extends UpdateTable {
+	private Connection con;
+	private static int pID;
+   
+	public U2(Connection con) {
+		this.con = con;
+	}
+   
+   public int getPID(){
+      return pID;
+   }
+   
+	public PreparedStatement prepareStatement() {
+		try {
+			return con.prepareStatement(qU2);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public String getStatement() {
+		return qU2;
+	}
+	
+	public void getPreparedStatement(BufferedReader br, PreparedStatement pst) throws IOException, NumberFormatException, SQLException {
+		
+		pst.setInt(1, U1.getPID());
+	}	
+}
