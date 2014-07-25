d8f87dc83d403fa3790f257fbad617a37315f029
Ian <Typhoan@users.noreply.github.com>
2014-07-25 13:10:51 -0500

    Update UpdateMenuOptions.java

--- project/menu/UpdateMenuOptions.java
+++ project/menu/UpdateMenuOptions.java
@@ -6,9 +6,4 @@ public interface UpdatetMenuOptions {
          + "\t1. Checkout Inpatient\n"
          + "\t2. Checkout outpatient\n"
          + "\t3. Back\n" + "Input command: ";
-         
-      public final String updateDiagnosisMenu = "\n______Update Patient Diagnosis______\n"
-         + "\t1. Update Inpatient Diagnosis\n"
-         + "\t2. Update outpatient Diagnosis\n"
-         + "\t3. Back\n" + "Input command: ";
 }
